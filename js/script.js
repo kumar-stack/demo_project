d9f9fa7a781a1d5f58c89c8586856131b903a95e
kumar-stack <gk690661@gmail.com>
2022-04-14 13:56:47 +0530

    navbar update

--- js/script.js
+++ js/script.js
@@ -6,28 +6,37 @@ $(document).ready(function () {
 
     });
 
-    if (window.innerWidth < 992) {
-           document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
-              everydropdown.addEventListener('hidden.bs.dropdown', function () {        
-              this.querySelectorAll('.submenu').forEach(function(everysubmenu){           
-                everysubmenu.style.display = 'none';
-              });
-          })
-        });
-      
-        document.querySelectorAll('.dropdown-menu a').forEach(function(element){
-          element.addEventListener('hover', function (e) {
-              let nextEl = this.nextElementSibling;
-              if(nextEl && nextEl.classList.contains('submenu')) {	
-                e.preventDefault();
-                if(nextEl.style.display == 'block'){
-                  nextEl.style.display = 'none';
-                } else {
-                  nextEl.style.display = 'block';
-                }
-      
-              }
-          });
-        })
+    if (window.innerWidth < 576){
+      $('.dropdown-toggle').click(function(){
+          $(".menu-drop").toggle();
+      });
+      $('.sub-item').click(function(){
+          $('.submenu').toggle();
+      });
     }
+
+    // if (window.innerWidth < 992) {
+    //        document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+    //           everydropdown.addEventListener('hidden.bs.dropdown', function () {        
+    //           this.querySelectorAll('.submenu').forEach(function(everysubmenu){           
+    //             everysubmenu.style.display = 'none';
+    //           });
+    //       })
+    //     });
+      
+    //     document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+    //       element.addEventListener('hover', function (e) {
+    //           let nextEl = this.nextElementSibling;
+    //           if(nextEl && nextEl.classList.contains('submenu')) {	
+    //             e.preventDefault();
+    //             if(nextEl.style.display == 'block'){
+    //               nextEl.style.display = 'none';
+    //             } else {
+    //               nextEl.style.display = 'block';
+    //             }
+      
+    //           }
+    //       });
+    //     })
+    // }
 });
